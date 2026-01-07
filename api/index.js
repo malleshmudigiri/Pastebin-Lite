38f531735371d5ad99cd8fd1158329386a3cd129
malleshmudigiri <malleshmudigiri91@gmail.com>
2026-01-07 11:54:13 +0530

    second commit

--- api/index.js
+++ api/index.js
@@ -1,13 +1,12 @@
 import cors from "cors";
 import express from "express";
-import healthRoute from "./routes/health.js";
-import pasteRoutes from "./routes/pastes.js";
-import { getNow } from "./utils/time.js";
-import redis from "./utils/redis.js";
-
+import healthRoute from "../routes/health.js";
+import pasteRoutes from "../routes/pastes.js";
+import { getNow } from "../utils/time.js";
+import redis from "../utils/redis.js";
 import dotenv from "dotenv";
-dotenv.config();
 
+dotenv.config();
 
 const app = express();
 
@@ -20,8 +19,6 @@ app.use("/api", pasteRoutes);
 app.get("/", (req, res) => {
   res.send("Pastebin Lite API running");
 });
-
-
 
 app.get("/p/:id", async (req, res) => {
   const raw = await redis.get(`paste:${req.params.id}`);
@@ -41,20 +38,6 @@ app.get("/p/:id", async (req, res) => {
 });
 
 
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-
-
-
-(async () => {
-  try {
-    await redis.ping();
-    console.log("Redis connected");
-  } catch (e) {
-    console.error(" Redis failed", e);
-  }
-})();
+export default function handler(req, res) {
+  return app(req, res);
+}
